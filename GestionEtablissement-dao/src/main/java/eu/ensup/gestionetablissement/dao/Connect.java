84ea863a45f70c4f73f9ca77c3b064ce493fdf55
asemin08 <60793285+asemin08@users.noreply.github.com>
2021-05-12 11:42:35 +0200

    Update Connect.java

--- GestionEtablissement-dao/src/main/java/eu/ensup/gestionetablissement/dao/Connect.java
+++ GestionEtablissement-dao/src/main/java/eu/ensup/gestionetablissement/dao/Connect.java
@@ -11,8 +11,8 @@ public class Connect
 {
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://vps-0c0ccce5.vps.ovh.net:3306/GestionEtablissement?serverTimezone=UTC";
-	private static final String USERNAME = "webs";
-	private static final String PASSWORD = "Admin08430*";
+	private static final String USERNAME = "identifiant";
+	private static final String PASSWORD = "mot-de-passe";
 	/**
 	 * Open an connention with the information in the class
 	 *

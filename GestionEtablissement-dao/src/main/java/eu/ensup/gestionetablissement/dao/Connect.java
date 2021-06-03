ea64b91fcd440b450b1a33515b24da0d680b131f
asemin08 <60793285+asemin08@users.noreply.github.com>
2021-06-03 10:39:40 +0200

    modif profile

--- GestionEtablissement-dao/src/main/java/eu/ensup/gestionetablissement/dao/Connect.java
+++ GestionEtablissement-dao/src/main/java/eu/ensup/gestionetablissement/dao/Connect.java
@@ -1,18 +1,21 @@
 package eu.ensup.gestionetablissement.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * The type Connect.
  */
 public class Connect
 {
-	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://url-de-votre-base-de-donne/nom-de-la-base?serverTimezone=UTC";
-	private static final String USERNAME = "identifiant";
-	private static final String PASSWORD = "mot-de-passe*";
+	private static String DRIVER = null;
+	private static String URL = null;
+	private static String USERNAME = null;
+	private static String PASSWORD = null;
 	/**
 	 * Open an connention with the information in the class
 	 *
@@ -20,6 +23,25 @@ public class Connect
 	 */
 	public static Connection openConnection() throws ExceptionDao
 	{
+		try (InputStream input = Connect.class.getClassLoader().getResourceAsStream("db.properties")) {
+
+			Properties prop = new Properties();
+
+			if (input == null) {
+				System.out.println("Impossible de charger les valeurs");
+			}
+			prop.load(input);
+
+			DRIVER = prop.getProperty("db.driver");
+			URL = prop.getProperty("db.url");
+			USERNAME = prop.getProperty("db.username");
+			PASSWORD = prop.getProperty("db.password");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		System.out.println(DRIVER + "..." + USERNAME +  "... " + PASSWORD + "..." + URL);
 		Connection cn = null;
 		try
 		{
